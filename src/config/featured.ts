5123e0d15478e3df6f935c3b5c6be3284e16eb56
stickfig8 <gusrb0482@gmail.com>
2026-01-02 02:08:10 +0900

    chore: edit featured post list

--- src/config/featured.ts
+++ src/config/featured.ts
@@ -1,6 +1,8 @@
 import { PostIdentifier } from "./types";
 
 export const FEATURED_POSTS:PostIdentifier[] = [
+  { category: "blog", slug: "simplypractice3" },
+  { category: "blog", slug: "simplypractice2" },
   { category: "blog", slug: "catsgotogedog8" },
   { category: "blog", slug: "catsgotogedog6" },
   { category: "blog", slug: "catsgotogedog3" },
